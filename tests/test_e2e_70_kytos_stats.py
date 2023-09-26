9a0e78a8a33eae402100cd6df341d9c26c129be6
Aldo Ortega <aortegay@fiu.edu>
2023-09-26 08:36:57 -0400

    Reverted change

--- tests/test_e2e_70_kytos_stats.py
+++ tests/test_e2e_70_kytos_stats.py
@@ -217,7 +217,6 @@ class TestE2EKytosStats:
 
     def test_025_packet_count_per_flow(self):
         """Test packet_count_per_flow""" 
-        time.sleep(10)
         api_url = KYTOS_STATS + '/flow/stats?dpid=00:00:00:00:00:00:00:01'  
         response = requests.get(api_url)
         assert response.status_code == 200, response.text
