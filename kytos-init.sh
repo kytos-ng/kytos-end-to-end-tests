72ca76b01ae4a5e4473e6809730a4ecc5d693dd3
HeriLFIU <hluna006@fiu.edu>
2024-01-11 13:56:24 -0500

    kytos-init.sh now searches for Napps directory or accepts an NAPPS_PATH environment variable

--- kytos-init.sh
+++ kytos-init.sh
@@ -4,18 +4,30 @@ set -xe
 
 # the settings below are intended to decrease the tests execution time (in fact, the time.sleep() calls
 # depend on the values below, otherwise many tests would fail)
-sed -i 's/STATS_INTERVAL = 60/STATS_INTERVAL = 7/g' /var/lib/kytos/napps/kytos/of_core/settings.py
-sed -i 's/CONSISTENCY_MIN_VERDICT_INTERVAL =.*/CONSISTENCY_MIN_VERDICT_INTERVAL = 60/g' /var/lib/kytos/napps/kytos/flow_manager/settings.py
-sed -i 's/LINK_UP_TIMER = 10/LINK_UP_TIMER = 1/g' /var/lib/kytos/napps/kytos/topology/settings.py
-sed -i 's/DEPLOY_EVCS_INTERVAL = 60/DEPLOY_EVCS_INTERVAL = 5/g' /var/lib/kytos/napps/kytos/mef_eline/settings.py
-sed -i 's/LLDP_LOOP_ACTIONS = \["log"\]/LLDP_LOOP_ACTIONS = \["disable","log"\]/' /var/lib/kytos/napps/kytos/of_lldp/settings.py
-sed -i 's/LLDP_IGNORED_LOOPS = {}/LLDP_IGNORED_LOOPS = {"00:00:00:00:00:00:00:01": \[\[4, 5\]\]}/' /var/lib/kytos/napps/kytos/of_lldp/settings.py
-sed -i 's/CONSISTENCY_COOKIE_IGNORED_RANGE =.*/CONSISTENCY_COOKIE_IGNORED_RANGE = [(0xdd00000000000000, 0xdd00000000000009)]/g' /var/lib/kytos/napps/kytos/flow_manager/settings.py
-sed -i 's/LIVENESS_DEAD_MULTIPLIER =.*/LIVENESS_DEAD_MULTIPLIER = 3/g' /var/lib/kytos/napps/kytos/of_lldp/settings.py
+if ! [ -z "$NAPPS_PATH" ]; then
+echo "NAPPS_PATH is specified."
+elif [[ $(find ../ -type d -path "**var/lib/kytos/napps" 2>/dev/null) ]]; then
+echo "NAPPS_PATH was not specified but $(find ../ -type d -path "**var/lib/kytos/napps" 2>/dev/null) Napps path found"
+DEFAULT_PATH="/var/lib/kytos/napps"
+PATH_FOUND=$(find ../ -type d -path "**var/lib/kytos/napps" 2>/dev/null)
+NAPPS_PATH=${PATH_FOUND/$DEFAULT_PATH/""}
+echo $NAPPS_PATH
+else
+echo "There is no NAPPS_PATH specified."
+NAPPS_PATH=""
+fi
+sed -i 's/STATS_INTERVAL = 60/STATS_INTERVAL = 7/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/of_core/settings.py
+sed -i 's/CONSISTENCY_MIN_VERDICT_INTERVAL =.*/CONSISTENCY_MIN_VERDICT_INTERVAL = 60/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/flow_manager/settings.py
+sed -i 's/LINK_UP_TIMER = 10/LINK_UP_TIMER = 1/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/topology/settings.py
+sed -i 's/DEPLOY_EVCS_INTERVAL = 60/DEPLOY_EVCS_INTERVAL = 5/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/mef_eline/settings.py
+sed -i 's/LLDP_LOOP_ACTIONS = \["log"\]/LLDP_LOOP_ACTIONS = \["disable","log"\]/' $NAPPS_PATH/var/lib/kytos/napps/kytos/of_lldp/settings.py
+sed -i 's/LLDP_IGNORED_LOOPS = {}/LLDP_IGNORED_LOOPS = {"00:00:00:00:00:00:00:01": \[\[4, 5\]\]}/' $NAPPS_PATH/var/lib/kytos/napps/kytos/of_lldp/settings.py
+sed -i 's/CONSISTENCY_COOKIE_IGNORED_RANGE =.*/CONSISTENCY_COOKIE_IGNORED_RANGE = [(0xdd00000000000000, 0xdd00000000000009)]/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/flow_manager/settings.py
+sed -i 's/LIVENESS_DEAD_MULTIPLIER =.*/LIVENESS_DEAD_MULTIPLIER = 3/g' $NAPPS_PATH/var/lib/kytos/napps/kytos/of_lldp/settings.py
 
 # increase logging to facilitate troubleshooting
 kytosd --help >/dev/null 2>&1  ## create configs at /etc/kytos from templates
-sed -i 's/WARNING/INFO/g' /etc/kytos/logging.ini
+sed -i 's/WARNING/INFO/g' $NAPPS_PATH/etc/kytos/logging.ini
 
 test -z "$TESTS" && TESTS=tests/
 test -z "$RERUNS" && RERUNS=2
