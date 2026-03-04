69f00d0e13bfeee3c5e1ebba7d139f9df6a85042
Aldo Ortega <55767214+Alopalao@users.noreply.github.com>
2026-03-04 10:59:33 -0500

    Applying seek to end

--- tests/test_e2e_90_kafka_events.py
+++ tests/test_e2e_90_kafka_events.py
@@ -63,7 +63,7 @@ class TestE2EKafkaEvents:
                 assert False
         # Create an EVC-creation event to send to Kafka
 
-        #await consumer.seek_to_end()
+        await consumer.seek_to_end()
         #await asyncio.sleep(4)
 
         evc_name = "Vlan_%s" % 902
