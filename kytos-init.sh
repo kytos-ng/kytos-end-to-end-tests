618a71c7f5f5d1dd288c63f0ddd42070de1ebdb2
Aldo Ortega <55767214+Alopalao@users.noreply.github.com>
2026-03-04 14:43:23 -0500

    Increased reruns to 100

--- kytos-init.sh
+++ kytos-init.sh
@@ -32,7 +32,7 @@ sed -i 's/keys: root,kytos,api_server,socket/keys: root,kytos,api_server,socket,
 echo -e "\n\n[logger_aiokafka]\nlevel: INFO\nhandlers:\nqualname: aiokafka" >> $NAPPS_PATH/etc/kytos/logging.ini
 
 test -z "$TESTS" && TESTS=tests/
-test -z "$RERUNS" && RERUNS=20
+test -z "$RERUNS" && RERUNS=100
 
 python3 scripts/wait_for_mongo.py 2>/dev/null
 python3 scripts/setup_kafka.py 2>/dev/null
